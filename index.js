5df8fdd494917bd2e87221fbe7f3f80964853530
结发受长生 <init_pro@aliyun.com>
2017-04-30 01:34:24 +0800

    配色修改 样式优化

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 require("./src/scss/base.scss");
-require("./src/scss/cnblog.scss");
+require("./src/scss/main.scss");
 require("./src/scss/layout.scss");
 require("./src/scss/nav.scss");
 // require("./src/scss/scroll.scss");
 require("./src/scss/article.scss");
 require("./src/scss/mobile.scss");
+
+// require("./src/scss/others");

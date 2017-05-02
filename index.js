7975df4287389505f33c7e8e997370dda2eef663
结发受长生 <init_pro@aliyun.com>
2017-05-02 17:32:03 +0800

    添加固定的顶部导航栏, 隐藏默认导航栏

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("./src/scss/base.scss");
 require("./src/scss/main.scss");
 require("./src/scss/layout.scss");
-require("./src/scss/nav.scss");
+require("./src/scss/header.scss");
 // require("./src/scss/scroll.scss");
 require("./src/scss/article.scss");
 require("./src/scss/mobile.scss");
